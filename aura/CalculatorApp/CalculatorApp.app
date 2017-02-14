d2c9b5b1d6ffd473ac035c5f86688c7cdb5b4537
rameshautorabit <ramesh.m@AR-LP2.tekrains.com>
2017-02-14 20:12:12 +0530

    Commit From AutoRABIT

--- aura/CalculatorApp/CalculatorApp.app
+++
@@ -1,3 +0,0 @@
-<aura:application >
-    <ACalculator:Calculator />
-</aura:application>
--- aura/RSSViewer/RSSViewerController.js
+++
@@ -1,9 +0,0 @@
-({
-	doInit : function(component, event, helper) {
-     	helper.getRSS(component);
-	},
-	refreshContent : function(component, event, helper) {
-    	document.getElementById(component.getGlobalId() + '_rssoutputcontainer').innerHTML = "Loading...";
-		helper.getRSS(component);
-	}    
-})
